1093b666029966759878fe74d844e171f3178807
Pavel <IU7.2@yandex.ru>
2021-02-09 00:25:47 +0300

    Новые задания

---
+++ tasks/10_square.js
@@ -0,0 +1,28 @@
+/**
+ * Задание. Написать функцию isSquare, которая возвращает true если число может быть образовано от целочисленного числа,
+ * путем возведения в квадрате
+ * Примеры
+ * -1  =>  false 
+ *  0  =>  true
+ *  3  =>  false
+ *  4  =>  true
+ * 25  =>  true
+ * 26  =>  false
+ */
+//  Реализация
+function isSquare(value) {
+    
+}
+
+
+
+
+
+
+//  Тесты
+console.assert(isSquare(-1), false);
+console.assert(isSquare( 0), true);
+console.assert(isSquare( 3), false);
+console.assert(isSquare( 4), true);
+console.assert(isSquare(25), true);
+console.assert(isSquare(26), false);
---
+++ tasks/12_sumOfSection.js
@@ -0,0 +1,27 @@
+/**
+ * Задание. Даны 2 числа. Необходимо посчитать сумму чисел, которые находятся в промежутке между этими числа
+ * включая граничные.
+ * Если числа совпадают то вернуть одно из них
+ * Примеры:
+ *   getSum(1, 0) == 1   // 1 + 0 = 1
+ *   getSum(1, 2) == 3   // 1 + 2 = 3
+ *   getSum(0, 1) == 1   // 0 + 1 = 1
+ *   getSum(1, 1) == 1   // 1
+ *   getSum(-1, 0) == -1 // -1 + 0 = -1
+ *   getSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+ */
+
+function getSum(a, b) {
+
+}
+
+//  Тесты
+console.assert(getSum(1, 0), 1)
+console.assert(getSum(1, 2), 3)
+console.assert(getSum(0, 1), 1)
+console.assert(getSum(1, 1), 1)
+console.assert(getSum(-1, 0), -1)
+console.assert(getSum(-1, 2), 2)
+
+
+
