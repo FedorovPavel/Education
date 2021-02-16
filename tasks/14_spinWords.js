93388ee4616d9987373239e637745b03b1bb631f
Pavel <IU7.2@yandex.ru>
2021-02-16 23:57:43 +0300

    Новые задания

---
+++ tasks/14_spinWords.js
@@ -0,0 +1,20 @@
+/**
+ * Задание. Напишите функцию, которая принимает строку из одного или нескольких слов и возвращает ту же строку, 
+ * но с перевернутыми словами из пяти или более букв. 
+ * Переданные строки будут состоять только из букв и пробелов. 
+ * Пробелы одиночные и только если в строке более одного слова.
+ * Примеры:
+ *      spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw"
+ *      spinWords( "This is a test") => returns "This is a test"
+ *      spinWords( "This is another test" )=> returns "This is rehtona test"
+ */
+
+ function spinWords(text) {
+
+ }
+
+//  Тесты
+console.assert(spinWords("Привет"), "тевирП");
+console.assert(spinWords("Привет товарищи войны"), "тевирП ищиравот ынйов");
+console.assert(spinWords("Это есть тест"), "Это есть тест");
+console.assert(spinWords("Это другой тест"), "Это йогурд тест");
---
+++ tasks/15_numberOfVowels .js
@@ -0,0 +1,17 @@
+/**
+ * Задание. Напишите функцию, которая принимает строку текста. 
+ * Она должна возвращать в качестве результата количество гласных букв в тексте. 
+ * Примеры:
+ *      numberOfVowels( "Привет мир" ) => returns 3
+ *      numberOfVowels( "Количество гласных") => returns 6
+ *      numberOfVowels( "Добрая половина выводов оказалась ошибочной" )=> returns 18
+ */
+
+ function numberOfVowels(text) {
+
+ }
+
+ //  Тесты
+console.assert(numberOfVowels("Привет мир"), 3);
+console.assert(numberOfVowels("Количество гласных"), 6);
+console.assert(numberOfVowels("Добрая половина выводов оказалась ошибочной"), 18);
